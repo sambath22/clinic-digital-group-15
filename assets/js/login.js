0d126eac92427fb9151d12d09f484809c9368063
Hor SengHab <horsenghab22@gmail.com>
2025-08-31 21:07:38 +0700

    Fix Icon on tap

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,7 +2,6 @@
 const isLoggedIn = localStorage.getItem("isLoggedIn");
 
 if (isLoggedIn === "true") {
-  // Remove Sign Up / Login buttons
   const btnContainer = document.querySelector(".d-flex.ms-3");
   if (btnContainer) {
     btnContainer.remove();
